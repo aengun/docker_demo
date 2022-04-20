ee000b262b6c4c680563cc944559a73a696d2d47
aengun <aengun@icloud.com>
2022-04-20 22:29:01 +0900

    feat: DockerFile 수정

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:15-ea-jdk-slim
-VOLUME /tmp
-COPY build/libs/docker-demo-0.0.1-SNAPSHOT.jar docekr-demo.jar
-ENTRYPOINT ["java","-jar","community-service.jar"]
+ARG JAR_FILE=build/libs/*.jar
+COPY ${JAR_FILE} app.jar
+ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/example/docker_demo/DockerDemoApplication.java
+++ src/main/java/com/example/docker_demo/DockerDemoApplication.java
@@ -2,6 +2,10 @@ package com.example.docker_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import java.time.ZonedDateTime;
 
 @SpringBootApplication
 public class DockerDemoApplication {
@@ -9,5 +13,11 @@ public class DockerDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DockerDemoApplication.class, args);
     }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void applicationReadyEventHandler() {
+        System.out.println(ZonedDateTime.now() + " Server Is Ready!!!");
+    }
+
 
 }
